225c942e48848bfb0ed95ab189c9ff544223753f
flash3105 <flash3105findme@gmail.com>
2025-04-14 12:11:16 +0200

    connectioning and requests now working

--- API/app.js
+++ API/app.js
@@ -1,9 +1,14 @@
 require('dotenv').config(); // this loads .env variables
+const cors = require('cors');
+
 
 const express = require('express');
 const connectDB = require('./config/db');
-
 const app = express();
+app.use(cors({
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080'],
+    credentials: true, 
+  }));
 
 // Connect to MongoDB
 connectDB();
--- UI/Login/Login.js
+++ UI/Login/Login.js
@@ -5,7 +5,7 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     const password = document.querySelector('#password').value;
 
     try {
-        const res = await fetch('/api/auth/login', {
+        const res = await fetch('http://localhost:5000/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
