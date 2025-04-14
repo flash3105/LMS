dc8131cecd072da4fae7519aa58c3b6d1c7168bd
flash3105 <flash3105findme@gmail.com>
2025-04-14 10:59:22 +0200

    added packages and routes for api

---
+++ API/Controllers/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
---
+++ API/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: { type: String, enum: ['student', 'instructor', 'admin'], default: 'student' }
+});
+
+module.exports = mongoose.model("User", UserSchema);
