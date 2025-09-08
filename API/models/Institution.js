b6bf8df9227f0ccef6484e438b9aefdd6c67ca8f
flash3105 <80648288+flash3105@users.noreply.github.com>
2025-09-08 22:45:10 +0200

    Update Institution.js

--- API/models/Institution.js
+++ API/models/Institution.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcryptjs'); 
 
 const InstitutionSchema = new mongoose.Schema({
     institutionName: { type: String, required: true },
